ff0b16141587e05e2adb8a82d35defebea16b0bd
John Doe <johndoe@example.com>
2024-07-29 18:55:28 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const button=document.querySelector(".buttoninput");
+function myfunction(){
+    
+    let message=document.querySelector("#message");
+    message.innerHTML="Result will be available after 5 seconds.....";
+
+}
+const mytimeout=setTimeout(createresult,5000);
+
+
+function createresult(result,callback){
+    document.getElementById("result").innerHTML="qui est esse ea molestias quasi exerciatiation repeliation of the date obater nsgtsj jshsdbdnd jdhdhdydfhf jdhdyfhfbfnf hdhyfnmfjfjf to be frack og rhw sgsbdnxjdgdbcb kdusdiaskhsnsc khdahdiy of the basic structure of the basic data to the world!!";
+    document.getElementById("result").style.backgroundColor="white";
+    document.getElementById("result").style.width="300px",height="300px";
+        
+}
+
+   
+
+
+    
+
+
